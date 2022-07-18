b456bc9de3d5dfc35f4e068c45d078044b1c335c
Atlantis3221 <stepatugarev98@gmail.com>
2022-07-18 14:28:06 +0700

    feat: accordeon logic

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 document.getElementById("burger").addEventListener("click", handleOpen);
 document.getElementById("close").addEventListener("click", handleClose);
+document.getElementById("openArrow").addEventListener("click", handleAccordeonOpen);
+document.getElementById("closeArrow").addEventListener("click", handleAccordeonClose);
 
 function handleOpen()  {
     document.getElementById("menu").style.display = "flex";
@@ -9,3 +11,10 @@ function handleClose() {
     document.getElementById("menu").style.display = "none";
 }
 
+function handleAccordeonOpen() {
+    document.getElementById("accordeon").style.display = "block";
+}
+
+function handleAccordeonClose() {
+    document.getElementById("accordeon").style.display = "none";
+}
